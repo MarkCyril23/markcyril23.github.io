f4191da46121b7cdeee698e95d31b428a62d0f18
MarkCyril23 <139624967+MarkCyril23@users.noreply.github.com>
2023-11-22 12:05:49 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,77 @@
+// Typewriter effect
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 200;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
+
+// Fade in while scrolling
+document.addEventListener("DOMContentLoaded", function() {
+    window.addEventListener("scroll", handleScroll);
+
+    function handleScroll() {
+        const elements = document.querySelectorAll(".fade-in");
+        elements.forEach(element => {
+            const elementRect = element.getBoundingClientRect();
+            const elementTop = elementRect.top;
+            const elementBottom = elementRect.bottom;
+            const windowHeight = window.innerHeight;
+            
+            if (elementBottom <= windowHeight || (elementTop + 150) <= windowHeight) {
+                element.classList.add("active");
+            }
+        });
+    }
+    handleScroll();
+});
